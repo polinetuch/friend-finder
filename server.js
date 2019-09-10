7a2b5b44e608ea484b0d363071561714ae268fca
Polinet <uchpolinet@gmail.com>
2019-09-10 17:10:32 +1000

    edited server file and made connection

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = 8282;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Table DATA
+// =============================================================
+var newUser = [
+  {
+    routeName: "",
+    name: "",
+    photo: "",
+    scores: "",
+  }
+];
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
