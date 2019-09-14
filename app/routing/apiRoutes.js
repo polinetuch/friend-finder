357032faf2d2b24ea8c5e763dd3b720156b6e7f8
Polinet <uchpolinet@gmail.com>
2019-09-14 23:28:20 +1000

    used for loop to calculate the differences of the scores to find best matching friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,19 +5,44 @@ function apiRoutes(app) {
     // display all tables
     app.get("/api/friends", function(req, res) {
         return res.json(friends);
-        console.log(friends)
+
     });
 
     app.post("/api/friends", function(req, res) {
+        var newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: []
+        };
 
-        friends.push(allFriends);
-        res.json(true);
-        var userOne = req.body;
-    
+        var scoreArray = [];
+        var friendOneScore = req.body.score
+        for (var i = 0; i < friendOneScore.length; i++) {
+            scoreArray.push(parseInt(friendOneScore[i]))
+        }
+        newFriend.score = scoreArray;
+        console.log(scoreArray);
 
-    })
-    
+        var scoreComparisonArr = [];
+        for (var i = 0; i < friends.length; i++) {
+            var currentScore = 0;
+            for (var j = 0; j < newFriend.score.length; j++) {
+                currentScore += Math.abs(newFriend.score[j] - friends[i].score[j]);
+                }
+                scoreComparisonArr.push(currentScore)
+            }
+        console.log(scoreComparisonArr)
 
+        var matchingFriend = 0;
+        for (var i = 1; i < scoreComparisonArr.length; i++) {
+            if (scoreComparisonArr[i] <= scoreComparisonArr[matchingFriend]) {
+                matchingFriend = i;
+            }
+        }
+
+        var compatibleFriend = friends[matchingFriend]
+        res.json(compatibleFriend)
+    });
 }
 
 module.exports = apiRoutes;
