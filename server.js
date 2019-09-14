9b43eea4d1515a8d82a84e92a2e5136831baa18d
Polinet <uchpolinet@gmail.com>
2019-09-14 18:43:04 +1000

    added process.env

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = 8282;
+var PORT = process.env.PORT || 8282;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
