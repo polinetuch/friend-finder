b98c3705e8b6b1b0fc99e05a7cee51e5a5e33c35
Polinet <uchpolinet@gmail.com>
2019-09-15 00:03:34 +1000

    added screenshot image

--- README.md
+++ README.md
@@ -2,8 +2,10 @@
 
 ### This is a compatibility-based "FriendFinder" application, which is basically a dating app. This full stack website will take in results from user's suverys, then compate their answers with those from the other users. The app will then display the name and picture of the user with the best overall match.
 
-#### Details on the app:
-    * 10 questions display in the survey
+#### Details on the app: https://find-your-friend-by-polinet.herokuapp.com/ 
+    10 questions display in the survey
+
+
     * Each answer is based on a scale from 1 to 5,
       which determines how much the user agrees or disagrees with the question.
     * Server.js handles all the response and request from client side. It also requires basic npm packages such as `express` and `path`
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,28 @@
 var friends = [
     {
     name: "Sheldon",
-    photo: "upon request",
+    photo: "https://wwwimage-secure.cbsstatic.com/thumbnails/photos/w425/cast/cast_bigbang_jimparsons.jpg",
     score: [5, 3, 3, 2, 4, 1, 3, 2, 1, 5]
     },
 {
     name: "Leonard",
-    photo: "you have to study applied physics",
+    photo: "https://pbs.twimg.com/profile_images/2654852680/87ee5ed912ea7364a29a380be9b22285_400x400.jpeg",
     score: [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
 },
 {
     name: "Raj",
-    photo: "i drink non-alcoholic drinks to pick up girls",
+    photo: "https://the-big-bang-theory.com/images/uploads/2/rs_35619e852e696869058.jpg",
     score: [5, 3, 4, 2, 1, 3, 5, 2, 1, 2]
+},
+{
+    name: "Penny",
+    photo: "https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Penny_bigbangtheory.jpg/250px-Penny_bigbangtheory.jpg",
+    score: [1, 5, 2, 3, 4, 2, 5, 3, 2, 1]
+}, 
+{
+    name: "Bernadette",
+    photo: "https://a1cf74336522e87f135f-2f21ace9a6cf0052456644b80fa06d4f.ssl.cf2.rackcdn.com/images/characters_opt/p-The-Big-Bang-Theory-Melissa-Rauchi.jpg",
+    score: [2, 3, 5, 2, 3, 1, 3, 4, 2, 1]    
 }
 ];
 
