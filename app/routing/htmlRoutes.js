55225738e0d48602dd37b0a8bd563233c0dd9ed8
Polinet <uchpolinet@gmail.com>
2019-09-14 18:59:52 +1000

    edited link to survey html file

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,7 +5,7 @@ function htmlRoutes(app) {
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
-  app.use(function(req, res) {
+  app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
